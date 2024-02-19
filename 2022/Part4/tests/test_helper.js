d3cc6a9a2dfc7f3bba8b833d964521f7a6e92a53
RedSquirrrel <anet1@rocketmail.com>
2024-02-19 13:16:28 +0000

    cleaning from some parts

--- 2022/Part4/tests/test_helper.js
+++ 2022/Part4/tests/test_helper.js
@@ -1,71 +1,71 @@
-const Blog = require('../models/blog');
-const User = require('../models/user');
+const Blog = require("../models/blog");
+const User = require("../models/user");
 
 const initialBlogs = [
   {
-    title: 'React patterns',
-    author: 'Michael Chan',
-    url: 'https://reactpatterns.com/',
+    title: "React patterns",
+    author: "Michael Chan",
+    url: "https://reactpatterns.com/",
     likes: 7,
   },
   {
-    title: 'Go To Statement Considered Harmful',
-    author: 'Edsger W. Dijkstra',
-    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    title: "Go To Statement Considered Harmful",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
     likes: 5,
   },
   {
-    title: 'Canonical string reduction',
-    author: 'Edsger W. Dijkstra',
-    url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+    title: "Canonical string reduction",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
     likes: 12,
   },
   {
-    title: 'First class tests',
-    author: 'Robert C. Martin',
-    url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
+    title: "First class tests",
+    author: "Robert C. Martin",
+    url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
     likes: 10,
   },
   {
-    title: 'TDD harms architecture',
-    author: 'Robert C. Martin',
-    url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
+    title: "TDD harms architecture",
+    author: "Robert C. Martin",
+    url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
     likes: 0,
   },
   {
-    title: 'Type wars',
-    author: 'Robert C. Martin',
-    url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
+    title: "Type wars",
+    author: "Robert C. Martin",
+    url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
     likes: 2,
   },
 ];
 
 const blogsInDB = async () => {
   const blogs = await Blog.find({});
-  return blogs.map(blog => blog.toJSON());
+  return blogs.map((blog) => blog.toJSON());
 };
 
 const aNewBlog = {
   title: "You Don't Know JS: Async & Performance",
-  author: 'Kyle Simpson',
-  url: 'https://github.com/getify/You-Dont-Know-JS/blob/1st-ed/async%20%26%20performance/ch3.md',
+  author: "Kyle Simpson",
+  url: "https://github.com/getify/You-Dont-Know-JS/blob/1st-ed/async%20%26%20performance/ch3.md",
   likes: 7,
   // user: [],
 };
 
 const newBlogWithNoLikes = {
-  title: 'If no likes',
-  author: 'test',
-  url: 'https://google.com',
+  title: "If no likes",
+  author: "test",
+  url: "https://google.com",
 };
 
 const noTitleAndUrl = {
-  author: 'Michael Chan',
+  author: "Michael Chan",
   likes: 7,
 };
 
 const nonExistingBlog = async () => {
-  const blog = new Blog({ title: 'React patterns', author: 'Michael Chan', url: 'https://reactpatterns.com/', likes: 7 });
+  const blog = new Blog({ title: "React patterns", author: "Michael Chan", url: "https://reactpatterns.com/", likes: 7 });
   await blog.save();
   await blog.remove();
 
@@ -73,14 +73,14 @@ const nonExistingBlog = async () => {
 };
 
 const initialUser = {
-  username: 'root',
-  name: 'test',
-  password: 'secret',
+  username: "root",
+  name: "test",
+  password: "secret",
 };
 
 const usersInDB = async () => {
   const users = await User.find({});
-  return users.map(user => user.toJSON());
+  return users.map((user) => user.toJSON());
 };
 
 module.exports = { initialBlogs, blogsInDB, newBlogWithNoLikes, noTitleAndUrl, aNewBlog, nonExistingBlog, usersInDB, initialUser };
